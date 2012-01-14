c790f42832f1856d49d7b48c6ce6935824e56005
Sergey Solovyev <se.solovyev@gmail.com>
2012-01-14 23:30:23 +0400

    moving files to project

--- src/main/java/org/solovyev/android/view/widgets/DirectionDragButton.java
+++ src/main/java/org/solovyev/android/view/widgets/DirectionDragButton.java
@@ -14,6 +14,7 @@ import android.text.TextPaint;
 import android.util.AttributeSet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.solovyev.android.R;
 import org.solovyev.common.NumberParser;
 import org.solovyev.common.utils.CollectionsUtils;
 import org.solovyev.common.utils.Point2d;
