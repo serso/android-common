bf74ca2b52dea144319e67b2b7c76121b4aef896
Sergey Solovyev <se.solovyev@gmail.com>
2012-02-08 14:09:56 +0400

    metro theme

--- src/main/java/org/solovyev/android/view/ColorButton.java
+++ src/main/java/org/solovyev/android/view/ColorButton.java
@@ -107,14 +107,8 @@ public class ColorButton extends Button {
 		feedbackPaint.setStyle(Style.STROKE);
 		feedbackPaint.setStrokeWidth(2);
 
-
-		if (CollectionsUtils.contains(getText().toString(), Arrays.asList("+", "-", "/", "×", "*", "∙"))) {
-			getPaint().setColor(resources.getColor(R.color.button_operator_text_color));
-		} else if (getText().toString().equals("C"))  {
-			getPaint().setColor(resources.getColor(R.color.button_ce_text_color));
-		} else {
-			getPaint().setColor(resources.getColor(R.color.button_text_color));
-		}
+		// todo serso: set attribute through style!
+		getPaint().setColor(resources.getColor(R.color.button_text_color));
 
 		animationStart = -1;
 
