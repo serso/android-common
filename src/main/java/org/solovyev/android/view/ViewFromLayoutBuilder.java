f51c8308f8ad1c80f4ab45c7d011c3ab14ad2304
Sergey Solovyev <se.solovyev@gmail.com>
2012-06-08 16:22:27 +0400

    View builder

--- src/main/java/org/solovyev/android/view/ViewFromLayoutBuilder.java
+++ src/main/java/org/solovyev/android/view/ViewFromLayoutBuilder.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * User: serso
@@ -19,6 +20,9 @@ public class ViewFromLayoutBuilder<V extends View> implements ViewBuilder<V> {
 
     private final boolean wholeLayout;
 
+    @Nullable
+    private LayoutInflater layoutInflater;
+
     private ViewFromLayoutBuilder(int layoutId, int viewId, boolean wholeLayout) {
         this.layoutId = layoutId;
         this.viewId = viewId;
@@ -26,25 +30,34 @@ public class ViewFromLayoutBuilder<V extends View> implements ViewBuilder<V> {
     }
 
     @NotNull
-    public static <V extends View> ViewBuilder<V> newInstance(int layoutId, int viewId) {
+    public static <V extends View> ViewFromLayoutBuilder<V> newInstance(int layoutId, int viewId) {
         return new ViewFromLayoutBuilder<V>(layoutId, viewId, false);
     }
 
     @NotNull
-    public static <V extends View> ViewBuilder<V> newInstance(int layoutId) {
+    public static <V extends View> ViewFromLayoutBuilder<V> newInstance(int layoutId) {
         return new ViewFromLayoutBuilder<V>(layoutId, 0, true);
+    }
+
+    public void setLayoutInflater(@Nullable LayoutInflater layoutInflater) {
+        this.layoutInflater = layoutInflater;
     }
 
     @NotNull
     @Override
     public V build(@NotNull Context context) {
-        final LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        LayoutInflater li = layoutInflater;
+        if (li == null) {
+            li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
         if (wholeLayout) {
             // if whole layout - just return view
-            return (V)layoutInflater.inflate(layoutId, null);
+            return (V)li.inflate(layoutId, null);
         } else {
             // else try to find view by id
-            final ViewGroup itemView = (ViewGroup) layoutInflater.inflate(layoutId, null);
+            final ViewGroup itemView = (ViewGroup) li.inflate(layoutId, null);
             return (V)itemView.findViewById(viewId);
         }
     }
