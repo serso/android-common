581ec7822aca7779bd5c60b09000eaab28b3cdde
Sergey Solovyev <se.solovyev@gmail.com>
2012-06-09 20:52:10 +0400

    JObject

---
+++ src/main/java/org/solovyev/common/JObject.java
@@ -0,0 +1,21 @@
+package org.solovyev.common;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * User: serso
+ * Date: 6/9/12
+ * Time: 8:50 PM
+ */
+public abstract class JObject implements JCloneable<JObject> {
+
+    @NotNull
+    @Override
+    public JObject clone() {
+        try {
+            return (JObject) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(e);
+        }
+    }
+}
