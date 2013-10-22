a1d9ec239ea13b56a01104e5d289e26d0fc92029
Sergey Solovyev <se.solovyev@gmail.com>
2013-10-22 13:32:18 +0400

    Travis CI integration

--- .travis_install_android.sh
+++ .travis_install_android.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn android update sdk --filter platform-tools,extra-android-support,add-on,android-16,sysimg-16,extra-google-play_billing,extra-google-m2repository,extra-google-google_play_services --no-ui --force --all
+spawn android update sdk --filter platform-tools,extra-android-support,add-on,android-17,sysimg-17,extra-google-play_billing,extra-google-m2repository,extra-google-google_play_services --no-ui --force --obsolete --all
 expect "Do you accept the license *:"
 send -- "y\r"
 expect "Do you accept the license *:"
