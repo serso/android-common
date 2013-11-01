c8199d21aede6baa46db1aaea260c9a3240564a1
serso <se.solovyev@gmail.com>
2013-11-02 01:04:16 +0400

    Travis CI: Android SDK artifacts added

--- .travis_install_android.sh
+++ .travis_install_android.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn android update sdk --filter platform-tools,build-tools-18.1.1,extra-android-support,add-on,android-17,sysimg-17,android-19,sysimg-19,extra-google-play_billing,extra-google-m2repository,extra-google-admob_ads_sdk,extra-google-analytics_sdk_v2,extra-google-gcm,extra-google-play_licensing,extra-google-google_play_services,extra-google-play_apk_expansion --no-ui --force --obsolete --all
+spawn android update sdk --filter tools,platform-tools,build-tools-19.0.0,extra-android-support,android-17,sysimg-17,android-19,sysimg-19,addon-google_apis-google-19,extra-google-play_billing,extra-google-m2repository,extra-google-admob_ads_sdk,extra-google-analytics_sdk_v2,extra-google-gcm,extra-google-play_licensing,extra-google-google_play_services,extra-google-play_apk_expansion,extra-android-m2repository --no-ui --force --obsolete --all
 expect "Do you accept the license *:"
 send -- "y\r"
 expect "Do you accept the license *:"
