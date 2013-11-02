4420030baa07f05a28175f7f5060077c944ee90e
serso <se.solovyev@gmail.com>
2013-11-02 19:46:00 +0400

    Travis CI: setting env variables from bash script

--- .travis_install_android.sh
+++ .travis_install_android.sh
@@ -7,7 +7,10 @@ if [ `uname -m` = x86_64 ]; then sudo apt-get install -qq libstdc++6:i386 lib32z
 wget -O android-sdk.tgz http://dl.google.com/android/android-sdk_r22.0.4-linux.tgz
 tar xzf android-sdk.tgz
 
+export ANDROID_HOME=$PWD/android-sdk-linux
 "export ANDROID_HOME=$PWD/android-sdk-linux" >> $HOME/.bash_profile
+
+export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
 "export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"  >> $HOME/.bash_profile
 
 sudo apt-get install expect
