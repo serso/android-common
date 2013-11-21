277480a0ddba7ffe6091a20d2e232b1b4d83cd71
Sergey Solovyev <se.solovyev@gmail.com>
2013-11-21 16:16:01 +0400

    Admob added to compile by maven sdk deployer

--- .travis_install_android_dependencies.sh
+++ .travis_install_android_dependencies.sh
@@ -6,4 +6,7 @@ git clone git://github.com/serso/maven-android-sdk-deployer.git
 cd ./maven-android-sdk-deployer/
 git checkout tags/api-19-mvn3.1
 mvn install -P4.2
+cd ./extras/admob
+mvn install
+cd ../..
 cd ..
